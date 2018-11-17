049ab949b7afc1010ff6b5625d1eaa6e927fd8db
Jesse Blacklock <jesse.blacklock@student.csulb.edu>
2018-11-17 04:05:10 -0800

    Camera Moves And Kills

---
+++ DreamTeam/Assets/CameraMover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+public class CameraMover : MonoBehaviour {
+
+    private int speed = 1;
+
+    void Start () {
+		
+	}
+
+    void Update () {
+        transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+    }
+}
---
+++ DreamTeam/Assets/cameraDeath.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+public class cameraDeath : MonoBehaviour
+{
+
+    private int speed = 1;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+    }
+}
