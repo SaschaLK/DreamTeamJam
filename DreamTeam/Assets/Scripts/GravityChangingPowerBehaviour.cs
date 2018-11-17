17e4951012b8fbedb99fb2dd423e480bae43cbf3
SaschaLK <SaschaL.Kufahl@gmail.com>
2018-11-16 21:13:54 -0800

    Gravity Controller With Button - Missing Timeout for Button

---
+++ DreamTeam/Assets/Scripts/GravityChangingPowerBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityChangingPowerBehaviour : MonoBehaviour {
+    //This script enables a player to change the Gravity
+    private void Update() {
+        if (Input.GetButtonDown("GravityChanger")) {
+            PlayerGravityManagerBehaviour.instance.ChangeGravity();
+        }
+    }
+}
