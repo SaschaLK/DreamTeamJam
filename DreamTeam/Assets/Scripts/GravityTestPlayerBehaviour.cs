54cb1397a7d146470a2fb8951885dd26fe2cdb2a
SaschaLK <SaschaL.Kufahl@gmail.com>
2018-11-16 20:54:35 -0800

    Implement Gravity Change (OnCollision for now) in Test Scene

---
+++ DreamTeam/Assets/Scripts/GravityTestPlayerBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityTestPlayerBehaviour : MonoBehaviour {
+    //THIS WHOLE SCRIPT IS FOR TEST PURPOSES ONLY
+    
+    private void OnCollisionEnter2D(Collision2D collision) {
+        PlayerGravityManagerBehaviour.instance.ChangeGravity();
+    }
+}
