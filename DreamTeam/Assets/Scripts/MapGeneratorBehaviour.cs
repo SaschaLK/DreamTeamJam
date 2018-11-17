00ce6f9d278d7f04f4d0d5f18de3f0c9b4d42ec9
SaschaLK <SaschaL.Kufahl@gmail.com>
2018-11-17 07:30:08 -0800

    Floor and ceiling Generation done - Diverging open

--- DreamTeam/Assets/Scripts/MapGeneratorBehaviour.cs
+++ DreamTeam/Assets/Scripts/MapGeneratorBehaviour.cs
@@ -10,42 +10,50 @@ public class MapGeneratorBehaviour : MonoBehaviour {
     public int renderedTileAmount;
     public List<GameObject> floorPrefabTiles = new List<GameObject>();
     public List<GameObject> ceilingPrefabTiles = new List<GameObject>();
-    public Vector3 floorCeilingOffset;
-    public Vector3 floorOffset;
+    public Vector3 SingleTileOffset;
+    public Vector3 startTileOffset;
     public float randomYOffsetRange;
     public Vector3 triggerPlaneOffset;
     public GameObject renderTriggerPlane;
 
+    private List<GameObject> ceilingTiles = new List<GameObject>();
     private List<GameObject> floorTiles = new List<GameObject>();
     private bool chunk;
     private int tileAmountHalf;
 
     private void Awake() {
         instance = this;
-
         tileAmountHalf = tileAmount / 2;
     }
 
     private void Start() {
         //Setting Trigger Plane position on game start. One time only offset of 9.
-        renderTriggerPlane.transform.position = new Vector3(triggerPlaneOffset.x - floorCeilingOffset.x / 2 + floorOffset.x + 9, 0, 0);
+        renderTriggerPlane.transform.position = new Vector3(triggerPlaneOffset.x - SingleTileOffset.x / 2 + startTileOffset.x + 9, 0, 0);
         renderTriggerPlane.SetActive(true);
 
-        for(int i = 0; i < tileAmount; i++) {
-            floorTiles.Add(Instantiate(floorPrefabTiles[Random.Range(0, floorPrefabTiles.Count - 1)], gameObject.transform));
-            if (i != 0) {
-                floorTiles[i].transform.position = floorTiles[i - 1].transform.position + floorCeilingOffset;
-            }
-        }
-        foreach(GameObject floorTile in floorTiles) {
-            floorTile.transform.position = floorTile.transform.position + floorOffset;
-        }
-        for(int i = 0; i < tileAmount; i++) {
-            if (i != 0) {
-                floorTiles[i].transform.position += new Vector3(0, Random.Range(-randomYOffsetRange, randomYOffsetRange) + (floorTiles[i-1].transform.position.y - floorTiles[i].transform.position.y), 0);
-            }
-        }
+        //ÌNSTANTIATE AND SETUP OF FLOOR AND CEILING
+        InstantiateSetup(ceilingTiles, ceilingPrefabTiles, startTileOffset.x, -startTileOffset.y);
+        InstantiateSetup(floorTiles, floorPrefabTiles, startTileOffset.x, startTileOffset.y);
+    }
 
+    private void InstantiateSetup(List<GameObject> tiles, List<GameObject> prefabTiles, float rowStartX, float rowStartY) {
+        //Instatiate Tiles and set in Row
+        for (int i = 0; i < tileAmount; i++) {
+            tiles.Add(Instantiate(prefabTiles[Random.Range(0, prefabTiles.Count - 1)], gameObject.transform));
+            if (i != 0) {
+                tiles[i].transform.position = tiles[i - 1].transform.position + SingleTileOffset;
+            }
+        }
+        //Offset Tilerow into Position
+        foreach (GameObject tile in tiles) {
+            tile.transform.position = tile.transform.position + new Vector3(rowStartX, rowStartY, 0);
+        }
+        //Waveform / Disturbance in Row
+        for (int i = 0; i < tileAmount; i++) {
+            if (i != 0) {
+                tiles[i].transform.position += new Vector3(0, Random.Range(-randomYOffsetRange, randomYOffsetRange) + (tiles[i - 1].transform.position.y - tiles[i].transform.position.y), 0);
+            }
+        }
     }
 
     public void GenerateMapSegment() {
@@ -54,25 +62,31 @@ public class MapGeneratorBehaviour : MonoBehaviour {
             chunk = !chunk;
             for (int i = 0; i < tileAmountHalf; i++) {
                 if(i == 0) {
-                    floorTiles[i].transform.position = new Vector3(floorTiles[i + tileAmountHalf].transform.position.x + triggerPlaneOffset.x, floorTiles[i + tileAmount - 1].transform.position.y + Random.Range(-randomYOffsetRange,randomYOffsetRange), 0);
+                    ceilingTiles[i].transform.position = new Vector3(ceilingTiles[tileAmountHalf].transform.position.x + triggerPlaneOffset.x, ceilingTiles[i + tileAmount - 1].transform.position.y + Random.Range(-randomYOffsetRange,randomYOffsetRange), 0);
+                    floorTiles[i].transform.position = new Vector3(floorTiles[tileAmountHalf].transform.position.x + triggerPlaneOffset.x, floorTiles[i + tileAmount - 1].transform.position.y + Random.Range(-randomYOffsetRange, randomYOffsetRange), 0);
                 }
                 else {
-                    floorTiles[i].transform.position = new Vector3(floorTiles[i - 1].transform.position.x + floorCeilingOffset.x, floorTiles[i-1].transform.position.y + Random.Range(-randomYOffsetRange, randomYOffsetRange), 0);
+                    LoadChunkRest(i);
                 }
             }
         }
         else {
             //Repositioning of the second half of the Tiles
             chunk = !chunk;
-
             for(int i = tileAmountHalf; i < tileAmount; i++) {
                 if(i == tileAmountHalf) {
-                    floorTiles[i].transform.position = new Vector3(floorTiles[i + tileAmountHalf - 1].transform.position.x + triggerPlaneOffset.x + floorCeilingOffset.x, floorTiles[tileAmountHalf - 1].transform.position.y + Random.Range(-randomYOffsetRange, randomYOffsetRange), 0);
+                    ceilingTiles[i].transform.position = new Vector3(ceilingTiles[i + tileAmountHalf - 1].transform.position.x + triggerPlaneOffset.x + SingleTileOffset.x, ceilingTiles[tileAmountHalf - 1].transform.position.y + Random.Range(-randomYOffsetRange, randomYOffsetRange), 0);
+                    floorTiles[i].transform.position = new Vector3(floorTiles[i + tileAmountHalf - 1].transform.position.x + triggerPlaneOffset.x + SingleTileOffset.x, floorTiles[tileAmountHalf - 1].transform.position.y + Random.Range(-randomYOffsetRange, randomYOffsetRange), 0);
                 }
                 else {
-                    floorTiles[i].transform.position = new Vector3(floorTiles[i - 1].transform.position.x + floorCeilingOffset.x, floorTiles[i - 1].transform.position.y + Random.Range(-randomYOffsetRange, randomYOffsetRange), 0);
+                    LoadChunkRest(i);
                 }
             }
         }
+    }
+    //TO DO: CLEAN UP / SIMPLIFICATION OF CODE ABOVE. DRY
+    private void LoadChunkRest(int i) {
+        ceilingTiles[i].transform.position = new Vector3(ceilingTiles[i - 1].transform.position.x + SingleTileOffset.x, ceilingTiles[i - 1].transform.position.y + Random.Range(-randomYOffsetRange, randomYOffsetRange), 0);
+        floorTiles[i].transform.position = new Vector3(floorTiles[i - 1].transform.position.x + SingleTileOffset.x, floorTiles[i - 1].transform.position.y + Random.Range(-randomYOffsetRange, randomYOffsetRange), 0);
     }
 }
