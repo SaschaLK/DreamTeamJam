5aba29a7cac5437fffcd355bce0420bc32b0f815
SaschaLK <SaschaL.Kufahl@gmail.com>
2018-11-17 05:50:05 -0800

    Working map Generation - No Bounds

--- DreamTeam/Assets/Scripts/MapGeneratorBehaviour.cs
+++ DreamTeam/Assets/Scripts/MapGeneratorBehaviour.cs
@@ -17,13 +17,18 @@ public class MapGeneratorBehaviour : MonoBehaviour {
     public GameObject renderTriggerPlane;
 
     private List<GameObject> floorTiles = new List<GameObject>();
+    private bool chunk;
+    private int tileAmountHalf;
 
     private void Awake() {
         instance = this;
+
+        tileAmountHalf = tileAmount / 2;
     }
 
     private void Start() {
-        renderTriggerPlane.transform.position = triggerPlaneOffset;
+        //Setting Trigger Plane position on game start. One time only offset of 9.
+        renderTriggerPlane.transform.position = new Vector3(triggerPlaneOffset.x - floorCeilingOffset.x / 2 + floorOffset.x + 9, 0, 0);
         renderTriggerPlane.SetActive(true);
 
         for(int i = 0; i < tileAmount; i++) {
@@ -37,7 +42,6 @@ public class MapGeneratorBehaviour : MonoBehaviour {
         }
         for(int i = 0; i < tileAmount; i++) {
             if (i != 0) {
-                //Random.Range(-randomYOffsetRange, randomYOffsetRange)
                 floorTiles[i].transform.position += new Vector3(0, Random.Range(-randomYOffsetRange, randomYOffsetRange) + (floorTiles[i-1].transform.position.y - floorTiles[i].transform.position.y), 0);
             }
         }
@@ -45,6 +49,30 @@ public class MapGeneratorBehaviour : MonoBehaviour {
     }
 
     public void GenerateMapSegment() {
+        if (!chunk) {
+            //Repositioning of the first half of the Tiles
+            chunk = !chunk;
+            for (int i = 0; i < tileAmountHalf; i++) {
+                if(i == 0) {
+                    floorTiles[i].transform.position = new Vector3(floorTiles[i + tileAmountHalf].transform.position.x + triggerPlaneOffset.x, floorTiles[i + tileAmount - 1].transform.position.y + Random.Range(-randomYOffsetRange,randomYOffsetRange), 0);
+                }
+                else {
+                    floorTiles[i].transform.position = new Vector3(floorTiles[i - 1].transform.position.x + floorCeilingOffset.x, floorTiles[i-1].transform.position.y + Random.Range(-randomYOffsetRange, randomYOffsetRange), 0);
+                }
+            }
+        }
+        else {
+            //Repositioning of the second half of the Tiles
+            chunk = !chunk;
 
+            for(int i = tileAmountHalf; i < tileAmount; i++) {
+                if(i == tileAmountHalf) {
+                    floorTiles[i].transform.position = new Vector3(floorTiles[i + tileAmountHalf - 1].transform.position.x + triggerPlaneOffset.x + floorCeilingOffset.x, floorTiles[tileAmountHalf - 1].transform.position.y + Random.Range(-randomYOffsetRange, randomYOffsetRange), 0);
+                }
+                else {
+                    floorTiles[i].transform.position = new Vector3(floorTiles[i - 1].transform.position.x + floorCeilingOffset.x, floorTiles[i - 1].transform.position.y + Random.Range(-randomYOffsetRange, randomYOffsetRange), 0);
+                }
+            }
+        }
     }
 }
