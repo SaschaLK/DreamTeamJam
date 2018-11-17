fd263ef1dc0ebc20aac3297ad0e0f90ef1539f4b
SaschaLK <SaschaL.Kufahl@gmail.com>
2018-11-17 01:08:03 -0800

    Map Generation work in Progress

---
+++ DreamTeam/Assets/Scripts/MapGeneratorBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGeneratorBehaviour : MonoBehaviour {
+
+    public static MapGeneratorBehaviour instance;
+
+    public int tileAmount;
+    public List<GameObject> floorTiles = new List<GameObject>();
+    public List<GameObject> ceilingTiles = new List<GameObject>();
+    public Vector3 floorCeilingOffset;
+    public Vector3 triggerPlaneOffset;
+    public GameObject renderTriggerPlane;
+
+    private void Awake() {
+        instance = this;
+    }
+
+    private void Start() {
+        renderTriggerPlane.transform.position = triggerPlaneOffset;
+        renderTriggerPlane.SetActive(true);
+    }
+
+    public void GenerateMapSegment() {
+        Debug.Log("Hello");
+    }
+}
